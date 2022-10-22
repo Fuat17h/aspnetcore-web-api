42b76ee3650840db244d0a1eb3adeb524bde4a45
Fuat17h <fuat17h@gmail.com>
2022-10-22 18:35:45 +0300

    building the api

---
+++ my-books/Data/AppDbInitializer.cs
@@ -0,0 +1,46 @@
+﻿using my_books.Data.Models;
+
+namespace my_books.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<AppDbContext>();
+
+                if (!context.Books.Any())
+                {
+                    context.Books.AddRange(new Book() { 
+                    
+                        Title = "1st Book Title",
+                        Description = "1st Book Description",
+                        IsRead = true,
+                        DateRead = DateTime.Now.AddDays(-10),
+                        Rate = 4, 
+                        Genre = "Biography",
+                        Author = "First Author",
+                        CoverUrl = "https....",
+                        DateAdded = DateTime.Now.Date 
+                    },
+                    new Book()
+                    {
+
+                        Title = "2nd Book Title",
+                        Description = "2nd Book Description",
+                        IsRead = false,
+                        Rate = 4,
+                        Genre = "Fiction",
+                        Author = "Second Author",
+                        CoverUrl = "https....",
+                        DateAdded = DateTime.Now
+
+                    });
+                    context.SaveChanges();
+                }
+
+            }
+        }
+    }
+}
--- my-books/Program.cs
+++ my-books/Program.cs
@@ -1,11 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using my_books.Data;
+using my_books.Data.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+
+//Configure DBContext with SQL
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(
+    builder.Configuration.GetConnectionString("DefaultConnectionString")));
+
+//Configure the Services
+builder.Services.AddTransient<BooksService>();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
 
 var app = builder.Build();
 
@@ -21,5 +39,8 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+//DBSeed
+AppDbInitializer.Seed(app);
 
 app.Run();
